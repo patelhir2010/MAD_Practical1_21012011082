0152af36e5cbf3e9e8fa9768b255c92bb2c6fafb
patelhir2010 <139534069+patelhir2010@users.noreply.github.com>
2023-07-19 19:04:37 +0530

    jj

---
+++ src/main/kotlin/pr1_2.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    // Integer to Double
+    val Int = 10
+    val Double = Int.toDouble()
+    println("Integer value:" + Int)
+    println("Double value (From Integer):" + Double)
+
+    val String = 10
+    val int = String.toInt()
+    println("String value: "+String)
+    println("Integer value (From String):"+int)
+
+    val str = 78
+    val double = str.toDouble()
+    println("String value: "+str)
+    println("Double value (From String: "+double)
+
+}
+
