51a554577bd151828623ddb64ea5c6321fc8984c
patelhir2010 <139534069+patelhir2010@users.noreply.github.com>
2023-07-19 18:35:04 +0530

    jj

--- src/main/kotlin/pr1_1.kt
+++ src/main/kotlin/pr1_1.kt
@@ -1,7 +1,20 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val a = 5               // Int
+    val b = 5.9      // Double
+    val c = 'D'          // Char
+    val d = "hello"        // Boolean
+    val e = true
+    val f = 96.35
+    val g = 783214556
+    val h = -5
+    val i = 127
+    println("Integer value: "+a)
+    println("Float Value: "+b)
+    println("Character value: "+c)
+    println("String value: "+d)
+    println("Boolean value: "+e)
+    println("Double value: "+f)
+    println("Long value: "+g)
+    println("Short value: "+h)
+    println("Byte value: "+i)
 }
