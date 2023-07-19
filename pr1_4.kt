3fcae32052bda269aa53dbf10c797ab9ed071ce0
patelhir2010 <139534069+patelhir2010@users.noreply.github.com>
2023-07-19 19:52:35 +0530

    jj

---
+++ pr1_4.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val number = 10
+
+    // Check if the number is even
+    if (number % 2 == 0) {
+        println("The number $number is even.")
+    } else {
+        println("The number $number is odd.")
+    }
+}
